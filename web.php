0c9512cfe43c548c2c7fa339cfdbabd6b15dca8c
tosihiro munekawa/ <pjdw8gtzr@gmail.com>
2020-01-20 11:44:31 +0900

    kadai

--- web.php
+++ web.php
@@ -15,10 +15,19 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::group(['prefix' => 'admin'], function() {
-    Route::get('news/create', 'Admin/NewsController@add');
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
+    Route::get('news/create', 'Admin\NewsController@add');
+    Route::post('news/create', 'Admin\NewsController@create'); # 追記
+
+    Route::get('profile/create', 'Admin\ProfileController@add');
+    Route::post('profile/create', 'Admin\ProfileController@create');
+
 });
 
-Route::get('admin/profile/create', 'Admin/ProfileController@add');
+Route::get('Admin/profile/edit', 'Admin\ProfileController@update')->middleware('auth');
 
-Route::get('admin/profile/edit', 'Admin/profileController@edit');
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
